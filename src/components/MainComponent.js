007fec122fe9220c90e0a9441ef708e9e70b95c1
anasvakyathodi <anasvakyathodi@gmail.com>
2020-08-08 10:34:33 +0530

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Layouts/Header";
 import Footer from "./Layouts/Footer";
 import Menu from "./MenuComponent";
 import Home from "./HomeComponent";
 import About from "./AboutComponent";
 import Contact from "./ContactComponent";
+import { COMMENTS } from "../context/Comments";
+import { PROMOTIONS } from "../context/Promotions";
+import { DISHES } from "../context/dishes";
+import { LEADERS } from "../context/leaders";
 // import Dishdetail from "./DishdetailComponent";
 import { Route, Switch, Redirect } from "react-router-dom";
 function App() {
+  const dishes = DISHES;
+  const comments = COMMENTS;
+  const promotions = PROMOTIONS;
+  const leaders = LEADERS;
+
+  function HomePage() {
+    return (
+      <Home
+        dish={dishes.filter((dish) => dish.featured)[0]}
+        promotion={promotions.filter((promo) => promo.featured)[0]}
+        leader={leaders.filter((leader) => leader.featured)[0]}
+      />
+    );
+  }
   return (
     <>
       <Header />
       <Switch>
-        <Route component={Home} exact path="/home" />
+        <Route component={HomePage} exact path="/home" />
         <Route component={Menu} exact path="/menu" />
         <Route component={About} exact path="/aboutus" />
         <Route component={Contact} exact path="/contactus" />
